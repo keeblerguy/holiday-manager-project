0388e8029f5151248c95ea97698da81834cf7807
keeblerguy <110788268+keeblerguy@users.noreply.github.com>
2022-08-29 10:12:08 -0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-weather_key = "d606fded8531398c5a6232601a46fbf0"
-json_loc = 'holidays.json'
-current_year = 2022
-scraped = "requests.get('https://www.timeanddate.com/holidays/us/2022')"
-config_loc = 'output.json'
